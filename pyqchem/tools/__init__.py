fix rotation function

pyqchem/tools/__init__.py
@@ -98,12 +98,14 @@ def rotate_coordinates(coordinates, angle, axis, atoms_list=None, center=(0, 0, 
     :param atoms_list: list of atoms to rotate (if None then rotate all)
     :return: rotated coordinates
     """
+
+    axis = np.array(axis) / np.linalg.norm(axis)  # normalize axis
     coordinates = np.array(coordinates) - np.array(center)
 
     cos_term = 1 - np.cos(angle)
-    rot_matrix = [[axis[0]**2*cos_term + np.cos(angle), axis[0]*axis[1]*cos_term - axis[2]*np.sin(angle), axis[0]*axis[2]*cos_term + axis[1]*np.sin(angle)],
-                  [axis[1]*axis[0]*cos_term + axis[2]*np.sin(angle), axis[1]**2*cos_term + np.cos(angle), axis[1]*axis[2]*cos_term + axis[0]*np.sin(angle)],
-                  [axis[2]*axis[0]*cos_term + axis[1]*np.sin(angle), axis[1]*axis[2]*cos_term + axis[0]*np.sin(angle), axis[2]**2*cos_term + np.cos(angle)]]
+    rot_matrix = [[axis[0]**2*cos_term + np.cos(angle),              axis[0]*axis[1]*cos_term - axis[2]*np.sin(angle), axis[0]*axis[2]*cos_term + axis[1]*np.sin(angle)],
+                  [axis[1]*axis[0]*cos_term + axis[2]*np.sin(angle), axis[1]**2*cos_term + np.cos(angle),              axis[1]*axis[2]*cos_term - axis[0]*np.sin(angle)],
+                  [axis[2]*axis[0]*cos_term - axis[1]*np.sin(angle), axis[1]*axis[2]*cos_term + axis[0]*np.sin(angle), axis[2]**2*cos_term + np.cos(angle)]]
 
     if atoms_list is not None:
         coordinates[atoms_list] = np.dot(coordinates[atoms_list], rot_matrix)