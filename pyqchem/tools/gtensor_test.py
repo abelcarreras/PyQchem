bcd5da9259ebcc601176722b8a54c17a9cade629
acebreiro <antonio.cebreiro@dipc.org>
2023-10-26 11:39:35 +0200

    generalization of g-tensor program

--- pyqchem/tools/gtensor_test.py
+++ pyqchem/tools/gtensor_test.py
@@ -9,7 +9,7 @@ from gtensor_parser import complete_procedure
 qchem_output = 'h2o_triplet.out'
 
 ppm = 0  # 0) g-shifts in ppt 1) g-shifts in ppm
-state_selection = 0  # 0) use "nstates" 1) use all states in "qchem_output"
+state_selection = 1  # 0) use "nstates" 1) use all states in "qchem_output"
 nstates = [1, 2, 3]  # States to be included when state_selection = 0
 
 #####################################
@@ -18,16 +18,11 @@ nstates = [1, 2, 3]  # States to be included when state_selection = 0
 g_shifts, totalstates = complete_procedure(qchem_output, nstates, state_selection, ppm)
 
 print("---------------------")
-print("  INPUT SECTION")
+print("      G-TENSOR ")
 print("---------------------")
-print("File selected: ", qchem_output)
-print("Number of states in output: ", totalstates)
-print("Selected states selected: ", nstates)
-
-print(" ")
-print("---------------------")
-print(" RAS-CI RESULTS")
-print("---------------------")
-print('g-factor (x y z dimensions):')
-print(np.round(g_shifts.real[0], 3), np.round(g_shifts.real[1], 3), np.round(g_shifts.real[2], 3))
+print("- File selected: ", qchem_output)
+print("- Number of states in output: ", totalstates)
+print("- Selected states selected: ", nstates)
+print('g-factor (x, y, z):', np.round(g_shifts.real[0], 3), ',', np.round(g_shifts.real[1], 3), ',',
+      np.round(g_shifts.real[2], 3))
 print('')
